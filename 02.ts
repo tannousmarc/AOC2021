b01b75dc335a585bef572c4661ff6304ef0017d6
Marc Tannous <mtannous@palantir.com>
2021-12-02 07:09:03 +0200

    day 2

---
+++ 02.ts
@@ -0,0 +1,27 @@
+import * as utils from "./utils"
+import * as fs from 'fs'
+
+fs.readFile('inputs/02.txt', 'utf8', (err: NodeJS.ErrnoException | null, data: string) => {
+    if(err) throw err;
+    
+    const instrs = data.split('\n');
+    let pos = [0, 0, 0];
+
+    for(let instr of instrs){
+        const [dir, val] = instr.split(" ");
+        switch (dir){
+            case "forward":
+                pos[1] += Number(val);
+                pos[0] += pos[2] * Number(val);
+                break;
+            case "down":
+                pos[2] += Number(val);
+                break
+            case "up":
+                pos[2] -= Number(val);
+                break;
+        }
+    }
+
+    console.log(pos[0] * pos[1]);
+});
